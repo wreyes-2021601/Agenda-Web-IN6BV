f87d4b2f9dbb9640787cecd29c38094d6f404775
Walter Reyes <wreyes-2021601@kinal.edu.gt>
2023-02-05 14:24:23 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+var inputTarea = document.getElementById("tarea");
+var btn = document.getElementById("agregar");
+var listado = document.getElementById("listado");
+var cantidad = document.getElementById("cantidad");
+
+
+var total = 0;
+
+
+btn.onclick = function(){
+    if(inputTarea.value ==""){
+        return;
+    }
+
+    var elemento = inputTarea.value;
+
+    var li = document.createElement("li");
+    li.textContent = elemento;
+
+    listado.appendChild(li);
+
+    total++;
+    cantidad.innerHTML = total;
+
+    var btnEliminar = document.createElement("span");
+    btnEliminar.textContent = "\u00d7";
+    li.appendChild(btnEliminar);
+
+    btnEliminar.onclick = fuction(){
+        li.remove();
+        total--;
+        cantidad.innerHTML = total;
+    }
+    inputTarea.value = "";
+}
